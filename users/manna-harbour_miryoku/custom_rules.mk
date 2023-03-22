c8ed502b604b688d6f693953ab15c76408a53026
Alessio Caiazza <nolith@abisso.org>
2023-03-22 11:41:38 +0100

    disable suspend power down only for cantor

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,4 +1,7 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
-NO_SUSPEND_POWER_DOWN =yes
+ifeq ($(strip $(KEYBOARD)), cantor)
+	# workaround for https://github.com/qmk/qmk_firmware/issues/16406
+	NO_SUSPEND_POWER_DOWN =yes
+endif
